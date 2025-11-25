9e44f0270c7f0a632d75684a460247c61e87d30c
Rikuto <mikadorikuto@gmail.com>
2025-11-25 20:20:09 +0900

    update the lecture

--- routes/shop.js
+++ routes/shop.js
@@ -8,7 +8,11 @@ const adminData = require('./admin');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-  res.render('shop');
+  const products = adminData.products;
+  // {prods: products} - Data object passed to the template
+  // prods: Variable name used in the template
+  // products: Actual data (array retrieved from adminData.products)
+  res.render('shop', {prods: products, docTitle: 'Shop'});
 });
 
 module.exports = router;
